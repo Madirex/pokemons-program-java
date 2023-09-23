315034e81ab0f6c25841ba3aa2eae82f6bd04277
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-09-23 22:29:43 +0200

    Added the export to csv

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,16 @@ repositories {
 }
 
 dependencies {
+
+
     implementation("org.projectlombok:lombok:1.18.28")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.h2database:h2:2.2.224")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.opencsv:opencsv:5.8")
+
 }
 
 tasks.test {
